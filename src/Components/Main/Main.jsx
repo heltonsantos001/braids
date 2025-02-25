c8632c42c0f42a07cf0dae7d9908798221ea4011
helton santos <heltonsanto001@gmail.com>
2025-02-25 16:16:05 -0300

    prototipo de cards braid finalizado

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,6 +4,8 @@ import FotoMain1 from "../../img/FotoMain1.jpg";
 import FotoMain2 from "../../img/FotoMain2.jpg"
 import FotoMain3 from "../../img/FotoMain3.jpg"
 import FotoMain4 from "../../img/FotoMain4.jpg"
+import FotoCard1 from "../../img/FotoCard1.jpg"
+
 
 export const Main = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -28,11 +30,59 @@ export const Main = () => {
               <h1>Braids</h1>
             </div>
           </div>
+
           <div id="ContainerCard">
-            <div className="CardBraid"></div>
-            <div className="CardBraid"></div>
-            <div className="CardBraid"></div>
-            <div className="CardBraid"></div>
+            <div className="CardBraid">
+               <div style={{ width: '300px', height: '300px', overflow: 'hidden' }}>
+                  <img src={FotoCard1}
+                       alt="Imagem"
+                       style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+                  />
+               </div>
+                <div id="TextCard">
+                 <h1>braids</h1>
+                 <p>300 reais</p>
+            </div>
+          </div>
+
+          <div className="CardBraid">
+               <div style={{ width: '300px', height: '300px', overflow: 'hidden' }}>
+                  <img src={FotoCard1}
+                       alt="Imagem"
+                       style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+                  />
+               </div>
+                <div id="TextCard">
+                 <h1>braids</h1>
+                 <p>300 reais</p>
+            </div>
+          </div>
+
+          <div className="CardBraid">
+               <div style={{ width: '300px', height: '300px', overflow: 'hidden' }}>
+                  <img src={FotoCard1}
+                       alt="Imagem"
+                       style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+                  />
+               </div>
+                <div id="TextCard">
+                 <h1>braids</h1>
+                 <p>300 reais</p>
+            </div>
+          </div>
+
+          <div className="CardBraid">
+               <div style={{ width: '300px', height: '300px', overflow: 'hidden' }}>
+                  <img src={FotoCard1}
+                       alt="Imagem"
+                       style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+                  />
+               </div>
+                <div id="TextCard">
+                 <h1>braids</h1>
+                 <p>300 reais</p>
+            </div>
+          </div>
           </div>
         </main>
       </div>
