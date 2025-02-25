3d2d5859a68faf87a0e52c51f7ea2e767a686323
helton santos <heltonsanto001@gmail.com>
2025-02-24 23:19:25 -0300

    Header and main

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./MainStyle.css";
+import FotoMain1 from "../../img/FotoMain1.jpg";
+import FotoMain2 from "../../img/FotoMain2.jpg"
+import FotoMain3 from "../../img/FotoMain3.jpg"
+import FotoMain4 from "../../img/FotoMain4.jpg"
+
+export const Main = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [FotoMain1, FotoMain2, FotoMain3, FotoMain4];
+  const changeSlide = (direction) => {
+    const newIndex = (currentIndex + direction + images.length) % images.length;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <>
+      <div>
+        <main>
+          <div className="carousel">
+            <button className="prev" onClick={() => changeSlide(-1)}>&#10094;</button>
+            <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="slide" />
+            <button className="next" onClick={() => changeSlide(1)}>&#10095;</button>
+          </div>
+          <div id="ContainerCard">
+            <div className="CardBraid"></div>
+            <div className="CardBraid"></div>
+            <div className="CardBraid"></div>
+            <div className="CardBraid"></div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
